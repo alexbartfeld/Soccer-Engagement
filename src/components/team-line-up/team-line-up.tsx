51f76b8866d605c7d5a4d7e205947b748070f4a9
Alex_Bartfeld <Welcome1>
2020-03-13 09:52:52 +0200

    add team line up

--- src/components/team-line-up/team-line-up.tsx
+++ src/components/team-line-up/team-line-up.tsx
@@ -1,31 +1,75 @@
 import {Component, Host, h, State} from '@stencil/core';
 
+function lineUpItem(player, color, setColor, isLeft) {
+  const c = setColor ? `linear-gradient(${isLeft ? '270' : '90'}deg, #000 20%, ${color} 100%)` : 'transparent';
+
+  return (<div class='player' style={{backgroundImage: c}}>
+    <span class='player__num'>{player.shirt_number}</span>
+    <span class='player__name'>{player.name}</span>
+  </div>)
+}
+
 @Component({
   tag: 'vff-team-line-up',
   styleUrl: 'team-line-up.css',
   shadow: true
 })
 export class TeamLineUp {
-  // private leftTeam = {formation: '', players: []};
-  // private rightTeam = {formation: '', players: []};
-  @State() lineUp = {home: {}, away: {}};
+  @State() teamsLineUp;
 
-  /*componentWillLoad() {
+  componentWillLoad() {
     return fetch('http://127.0.0.1:8080/teams_lineup.json')
       .then(response => response.json())
       .then(json => {
-        this.lineUp = json;
-        this.leftTeam = this.lineUp.home;
-        this.rightTeam = this.lineUp.away;
+        this.teamsLineUp = json;
       }).catch(err => {
         return err;
       });
-  }*/
+  }
 
   render() {
+    const leftTeam = this.teamsLineUp.home;
+    const rightTeam = this.teamsLineUp.away;
+    const leftTeamPlayers = leftTeam.players;
+    const rightTeamPlayers = rightTeam.players;
+    const leftTeamActivePlayers = leftTeamPlayers.filter(player => player.substitution === '0');
+    const rightTeamActivePlayers = rightTeamPlayers.filter(player => player.substitution === '0');
+    const leftTeamSubstitutes = leftTeamPlayers.filter(player => player.substitution === '1');
+    const rightTeamSubstitutes = rightTeamPlayers.filter(player => player.substitution === '1');
+    const leftTeamColor = leftTeam.team_color;
+    const rightTeamColor = rightTeam.team_color;
     return (
       <Host>
         <vff-formation/>
+        <div id='line-up'>
+          <div class='left'>
+            {
+              leftTeamActivePlayers.map((player, i) => {
+                return lineUpItem(player, leftTeamColor, (i % 2 === 0), true);
+              })}
+          </div>
+          <div class='right'>
+            {
+              rightTeamActivePlayers.map((player, i) => {
+                return lineUpItem(player, rightTeamColor, (i % 2 === 0), false);
+              })}
+          </div>
+        </div>
+        <div id='substitutes__title'>Substitutes</div>
+        <div id='substitutes'>
+          <div class='left'>
+            {
+              leftTeamSubstitutes.map((player, i) => {
+                return lineUpItem(player, leftTeamColor, (i % 2 === 0), true);
+              })}
+          </div>
+          <div class='right'>
+            {
+              rightTeamSubstitutes.map((player, i) => {
+                return lineUpItem(player, rightTeamColor, (i % 2 === 0), false);
+              })}
+          </div>
+        </div>
       </Host>
     );
   }
--- src/components/vff-sport-fan/vff-sport-fan.tsx
+++ src/components/vff-sport-fan/vff-sport-fan.tsx
@@ -1,4 +1,4 @@
-import {Host, Component, h} from '@stencil/core';
+import {Host, Component, h, Element} from '@stencil/core';
 import {routes} from "../../routes/routes";
 
 @Component({
@@ -7,6 +7,7 @@ import {routes} from "../../routes/routes";
   shadow: true
 })
 export class VffSportFan {
+  @Element() el: HTMLElement;
 
   render() {
     return (
@@ -17,8 +18,8 @@ export class VffSportFan {
             <stencil-router>
               <stencil-route-switch scrollTopOffset={0}>
                 <stencil-route url={routes.TIME_LINE} component='vff-time-line' exact={true}/>
-                <stencil-route url={routes.FORMATION} component='vff-formation'/>
                 <stencil-route url={routes.STATS} component='vff-stats'/>
+                <stencil-route url={routes.FORMATION} component='vff-team-line-up'/>
                 <stencil-route component="vff-time-line"/>
               </stencil-route-switch>
             </stencil-router>
